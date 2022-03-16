b34d0d2c169a0100dd4e3db2adfca9254b5fa5ed
fibss <fibster03@gmail.com>
2022-03-16 20:45:03 +0300

    HW1  fixed

--- AllHW/AllHW/HW1.cs
+++ AllHW/AllHW/HW1.cs
@@ -4,11 +4,21 @@
     {
         public static void FindResultofDivision(int dividend, int divider, out int resultOfDivision, out int remainderOfTheDivision)
         {
+            if (b == 0)
+            {
+                throw new Exception("b must be > 0");
+            }
+
             resultOfDivision = dividend / divider;
             remainderOfTheDivision = dividend % divider;
         }
        public static double DivisionCalculation(double a, double b)
         {
+            if (a == b)
+            {
+                throw new Exception("a must not be equal b");
+            }
+
             double result = (5 * a + b * b) / (b - a);
             return result;
         }
@@ -21,12 +31,18 @@
         {
             if (a == 0 || b == 0 || c == 0)
             {
-                 throw new Exception("Ошибка. Введите числа, отличные от нуля");
-            }    
+                 throw new Exception("a must be > 0");
+            } 
+            
             return (double) (c - b) / a;
         }
         public static string DeriveTheEquationOfAStraightLinePassingThroughTwoPoints(double x1, double x2, double y1, double y2)
         {
+            if (x1 == x2)
+            {
+                throw new Exception(" x1 must not be equal x2");
+            }
+
             double a = (x2 - x1) / (y2 - y1);
             double b = y2 - a * x2;
             string answer = $"y = {a} * x + { b}";
